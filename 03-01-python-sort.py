15f21bc400dd653a9577e28c405da00bdd7baf60
Hiroshi Nakatsukasa <erdes.piro@gmail.com>
2018-07-04 13:59:05 +0900

    修正

--- 03-01-python-sort.py
+++ 03-01-python-sort.py
@@ -4,10 +4,13 @@ import csv,operator
 
 f=open("task.csv")
 reader=csv.reader(f)
+print(" ".join(next(reader)))   # next関数で一行目だけ先に処理。
+                                # join関数は文字列に対する処理。""内の文字を間に
+                                # 挟みながらリストに含まれるそれぞれの要素を連結。
 
 sortlize_reader=sorted(reader,key=operator.itemgetter(1))
-
+                                # 残りの行に対してソート
 for row in sortlize_reader:
-    print(row)
+    print(" ".join(row))
 
 f.close()
